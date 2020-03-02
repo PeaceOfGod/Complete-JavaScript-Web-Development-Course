6d9ba489d0ee5f27d357153d83852659e98a9624
Peace BAKARE <peace.bakare@intranet.com>
2020-03-02 13:51:18 +0100

    Increasing, decreasing and reset buttons added

--- src/Application.js
+++ src/Application.js
@@ -1,36 +1,42 @@
 import React, {Component} from 'react';
+import { blue } from 'color-name';
 
 class Application extends Component {
     constructor(props){
         super (props);
-    }
 
-    componentWillMount(props, state){
-
-    }
-
-    componentDidMount(props, state){
-        console.log("Mounted with", props, state);
-    }
-
-    componentWillReceiveProps(props){
-
-    }
-
-    componentWillUpdate(props, state){
-        if (this.props.name !== props.name) {
-            //Do something;
+        this.state = {
+            count: 0
         }
     }
 
-    componentDidUpdate(props, state){
-
+    increaseCount = () => {
+        this.setState({count: this.state.count + 1});
+    }
+    
+    resetFunction = () => {
+        this.setState({count: 0});
     }
 
+    decreaseCount = () => {
+        this.setState({count: this.state.count - 1});
+    }
+
+    componentDidUpdate(props, state){
+        console.log("Updated from ", state, " to ", this.state);
+    }
     render () {
-        var name = "Peace";
+        let {count} = this.state;
         return (
-            <h1>Hello, {name}</h1>
+            <div>
+                <h1>You clicked the button {count} times</h1>
+                <span>
+                    <button onClick={(e) => this.increaseCount()}> Increase </button>
+                    <button onClick={(e) => this.resetFunction()}> Reset </button>
+                    <button onClick={(e) => this.decreaseCount()}> Decrease </button>
+                </span>
+            </div>
+            
         );
     }
 }
